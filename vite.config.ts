589a449cc9a828113b3e7d6b3a76860e464605c4
jokiefer <jonas.kiefer@live.com>
2024-04-30 17:54:24 +0200

    add react test example

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,11 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
+    test: {
+        globals: true,
+        environment: 'jsdom',
+        setupFiles: './tests/setup.js'
+    },
     define: {
         'process.env': process.env,
     },
@@ -11,6 +16,7 @@ export default defineConfig({
         host: true,
     },
     base: './',
+    
     // resolve: {
     //     alias: {
     //         '@api-platform/admin/src/InputGuesser': './node_modules/@api-platform/admin/src/InputGuesser'
