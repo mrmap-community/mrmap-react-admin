89877b98daf15495068d1ad5567607c4164b7b6c
jokiefer <jonas.kiefer@live.com>
2024-05-15 20:06:24 +0200

    Update launch.json and LayerTree.spec.tsx

--- src/components/LayerTree/LayerTree.spec.tsx
+++ src/components/LayerTree/LayerTree.spec.tsx
@@ -1,13 +1,9 @@
-import { render, screen, fireEvent, within, act } from '@testing-library/react';
+import { render, screen, fireEvent, within, waitFor } from '@testing-library/react';
 import { describe, it, expect } from 'vitest';
 import LayerTree from './LayerTree';
 import { OwsContextBase } from '../../react-ows-lib/ContextProvider/OwsContextBase';
 import {karteRpFeatures as testdata} from '../../ows-lib/OwsContext/tests/data'
-
-
-
 import {ReactNode} from 'react'
-
 
 
 const getKarteRpFeatures = () => {
@@ -46,21 +42,57 @@ describe('LayerTree', () => {
     expect(screen.getByText('Wald')).toBeInTheDocument();
   });
 
-  it.todo('Wald 0 can be moved as left sibling of Wald', () => {
-    render(<LayerTree initialExpanded={['/0', '/0/1']}/>, {wrapper: MapViewerBaseWrapper})
+  it('Wald 0 can be dragged as left sibling of Wald', async () => {
+  
+    const {baseElement} = render(<LayerTree initialExpanded={['/0', '/0/1']}/>, {wrapper: MapViewerBaseWrapper})
     const karteRp = screen.getByText('Karte RP');
     const wald0 = screen.getByText('Wald 0');
-    const wald = screen.getByText("Wald");
 
-    act(()=>{
-      fireEvent.dragStart(wald0);
-      fireEvent.dragEnter(wald);
-      fireEvent.dragOver(wald);
-      fireEvent.drop(wald);
-    })
+    const wald0Rect = wald0.getBoundingClientRect()
+    const karteRpRect = karteRp.getBoundingClientRect()
+    const itemOffset = { 
+      top: wald0Rect.top + window.scrollY, 
+      left: wald0Rect.left + window.scrollX
+    };
 
-    //screen.debug(undefined, 20000)
-    expect(within(karteRp).getByText('Wald 0')).toBeInTheDocument();
-    expect(within(wald).getByText('Wald 0')).toBeNotInTheDocument();
+    const targetOffset = { 
+      top: karteRpRect.top + window.scrollY, 
+      left: karteRpRect.left + window.scrollX, 
+      bubbles: true,
+      cancelable: true,
+      button: 0
+    };
+
+    const startCords = { 
+      clientX: itemOffset.left, 
+      clientY: itemOffset.top,
+      bubbles: true,
+      cancelable: true,
+      button: 0
+    };
+    const endCords = { 
+      clientX: targetOffset.left, 
+      clientY: targetOffset.top,
+      bubbles: true,
+      cancelable: true,
+      button: 0
+    };
+
+
+
+    fireEvent.mouseDown(wald0, startCords);
+    fireEvent.dragStart(wald0, startCords);
+    fireEvent.drag(wald0, endCords);
+    fireEvent.dragOver(karteRp,endCords);
+    
+    await waitFor(()=> expect(screen.getByText('Wald 0')).toBeInTheDocument());
+
+    fireEvent.drop(wald0, endCords);
+    //fireEvent.mouseUp(wald0, endCords);
+
+    // screen.debug(undefined, 20000)
+
+    //expect(within(karteRp).getByText('Wald 0')).toBeInTheDocument();
+    // expect(within(wald).getByText('Wald 0')).toBeNotInTheDocument();
   });
 });
